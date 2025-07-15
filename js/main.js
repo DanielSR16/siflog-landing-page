8a93fe610cfbc4e71e50a7113d067898b9d9f3ef
DanielSR16 <danielsan.robles16@gmail.com>
2025-07-14 22:48:37 -0600

    implementacion js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("Landing Page cargada correctamente.");
+//form general contact
+document.getElementById("generalContact").addEventListener("submit",function(event){
+    event.preventDefault()
+
+    const name = document.getElementById("name").value;
+    const companyName = document.getElementById("companyName").value;
+    const email = document.getElementById("email").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+    const message = document.getElementById("message").value;
+
+    console.log({
+        name,
+        companyName,
+        email,
+        phoneNumber,
+        message
+    })
+ 
+})
