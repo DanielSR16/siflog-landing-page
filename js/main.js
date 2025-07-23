5783ab80c21424a2e9207646d8a6a5d36bd9c1d8
DanielSR16 <danielsan.robles16@gmail.com>
2025-07-22 19:11:46 -0600

    fix formulario

--- js/main.js
+++ js/main.js
@@ -1,51 +1,24 @@
 
+
+
 const generalContact = document.getElementById("generalContact");
 
 generalContact.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    //nombre
+
     const name = document.getElementById("nameContact").value;
-    //nombre de la empresa
-    const companyName = document.getElementById("companyNameContact").value;
-    //correo
+  
+    const company = document.getElementById("companyNameContact").value;
+
+    const type = document.getElementById("selectTypeFeedback").value;
+    
     const email = document.getElementById("emailContact").value;
-    //telefono
-    const phoneNumber = document.getElementById("phoneNumberContact").value;
-    //mensaje
+  
+    const phone = document.getElementById("phoneNumberContact").value;
+
     const message = document.getElementById("messageContact").value;
 
-    console.log({
-        name,
-        companyName,
-        email,
-        phoneNumber,
-        message
-    });
-
-
+    console.log({ name, company,type,email,phone,message })
     generalContact.reset();
-});
-
-
-
-
-const feedback = document.getElementById("feedback");
-
-feedback.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    //nombre
-    const name = document.getElementById("nameFeedback").value;
-    //select
-    const select = document.getElementById("selectTypeFeedback").value;
-    //correo
-    const email = document.getElementById("emailFeedback").value;
-    //area
-    const area = document.getElementById("selectAreaFeedback").value;
-    //detalle
-    const detail = document.getElementById("detailsFeedback").value;
-
-    console.log({ name, select,email,area,detail })
-    feedback.reset();
 });
